f094f3546e964a3b51189cbb981a8b5bbca4809b
spurgason <shanepurgason.98@gmail.com>
2021-12-21 14:39:24 -0500

    added offline functionality

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
+
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
@@ -11,6 +13,17 @@ const Cart = () => {
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
     }
+
+    useEffect(() => {
+      async function getCart() {
+        const cart = await idbPromise('cart', 'get');
+        dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+      };
+    
+      if (!state.cart.length) {
+        getCart();
+      }
+    }, [state.cart.length, dispatch]);
 
     if (!state.cartOpen) {
         return (
@@ -22,6 +35,8 @@ const Cart = () => {
         );
     }
 
+   
+
     function calculateTotal() {
         let sum = 0;
         state.cart.forEach(item => {
@@ -29,6 +44,8 @@ const Cart = () => {
         });
         return sum.toFixed(2);
     }
+
+
 
     return (
         <div className="cart">
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -9,6 +9,8 @@ import {
   UPDATE_PRODUCTS,
 } from '../utils/actions';
 import Cart from '../components/Cart';
+import { idbPromise } from "../utils/helpers";
+
 
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
@@ -24,12 +26,17 @@ function Detail() {
   const { products, cart } = state;
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === id);
-
+    const itemInCart = cart.find((cartItem) => cartItem._id === id)
+  
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+      idbPromise('cart', 'put', {
+        ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
@@ -37,27 +44,48 @@ function Detail() {
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 }
       });
+      // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
-  };
+  }
 
   const removeFromCart = () => {
     dispatch({
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+  
+    // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
 
   useEffect(() => {
+    // already in global store
     if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
-    } else if (data) {
+      setCurrentProduct(products.find(product => product._id === id));
+    } 
+    // retrieved from server
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
-      })
+      });
+  
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
